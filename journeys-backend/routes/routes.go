5819a6c767b4977e6c33532e78fa77201c2be8fc
Arfiannn <arfianpndg@gmail.com>
2025-12-18 03:20:19 +0800

    feat(cors): setup static allowed origins for react and flutter

--- journeys-backend/routes/routes.go
+++ journeys-backend/routes/routes.go
@@ -11,7 +11,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8000", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
