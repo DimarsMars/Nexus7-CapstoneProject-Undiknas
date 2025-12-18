de42c65c669ee0227ec0d63910ac2ab56bf467e1
DimarsMars <dimasrangga620@gmail.com>
2025-12-18 22:18:03 +0800

    Integrasi card categories yang diambil dari database

--- frontend-web/src/components/PlanCategorySection.jsx
+++ frontend-web/src/components/PlanCategorySection.jsx
@@ -1,23 +1,44 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import apiService from '../services/apiService';
+import { useAuth } from '../context/AuthContext';
 
-const PlansCategory = ({categories}) => {
+const PlansCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await apiService.getCategories();
+        setCategories(response.data || []);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    if (user) {
+      fetchCategories();
+    }
+  }, [user]);
+
   return (
     <section className="px-5 my-16">
       <div className="max-w-7xl mx-auto">
         <h2 className="text-3xl font-bold text-center text-black mb-10">Plan’s Category</h2>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-6">
-          {categories.map((item) => (
-            <div key={item.id} className="flex flex-col items-center group cursor-pointer">
+          {categories.slice(0, 4).map((item) => (
+            <div key={item.category_id} className="flex flex-col items-center group cursor-pointer">
               <div className="w-full h-32 md:h-40 rounded-lg overflow-hidden shadow-sm mb-2">
                 <img 
-                    src={item.image} 
-                    alt={item.title} 
+                    src={`data:image/jpeg;base64,${item.image}`} 
+                    alt={item.name} 
                     className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
                 />
               </div>
 
-              <p className="text-md font-medium text-gray-800 group-hover:text-black">{item.title}</p>
+              <p className="text-md font-medium text-gray-800 group-hover:text-black">{item.name}</p>
             </div>
           ))}
         </div>
