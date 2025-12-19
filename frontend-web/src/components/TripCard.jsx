d08220193a7c818de0675f12c036fdce14537ed3
DimarsMars <dimasrangga620@gmail.com>
2025-12-20 01:30:24 +0800

    Mengedit tampilan gambar menjadi object-cover

--- frontend-web/src/components/TripCard.jsx
+++ frontend-web/src/components/TripCard.jsx
@@ -10,7 +10,7 @@ const TripCard = ({ id, image, title, author, rating, className }) => {
       <img 
         src={image} 
         alt={title} 
-        className="w-full h-full object-top transition-transform duration-300 group-hover:scale-110" 
+        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110" 
       />
 
       <div className="absolute inset-0 bg-linear-to-t from-black/90 via-black/20 to-transparent"></div>
