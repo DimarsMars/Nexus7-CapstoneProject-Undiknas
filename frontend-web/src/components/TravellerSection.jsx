4e1c50af2c770aec3e4e4a33da56a15b12ce50e0
DimarsMars <dimasrangga620@gmail.com>
2025-12-25 01:22:46 +0800

    Integrasi TravellerPage menggunakan data dari database

--- frontend-web/src/components/TravellerSection.jsx
+++ frontend-web/src/components/TravellerSection.jsx
@@ -1,6 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import apiService from "../services/apiService";
 
-const TravellerSection = ({ travellers }) => {
+const TravellerSection = () => {
+  const [travellers, setTravellers] = useState([]);
+
+  useEffect(() => {
+    const fetchTravellers = async () => {
+      try {
+        const response = await apiService.getAllUsers();
+  
+        if (response.data && Array.isArray(response.data)) {
+             setTravellers(response.data);
+        } else if (response.data && response.data.data && Array.isArray(response.data.data)) {
+             setTravellers(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching travellers:", error);
+      }
+    };
+
+    fetchTravellers();
+  }, []);
+
   return (
     <section className="px-5 my-16">
       <div className="max-w-7xl mx-auto">
@@ -10,16 +32,16 @@ const TravellerSection = ({ travellers }) => {
         <div className="grid grid-cols-2 md:grid-cols-5 gap-8 md:gap-2 justify-items-center">
             
             {travellers?.map((person) => (
-                <div key={person.id} className="flex flex-col items-center group cursor-pointer">
+                <div key={person.user_id} className="flex flex-col items-center group cursor-pointer">
                     <div className="w-32 h-32 md:w-40 md:h-40 rounded-full overflow-hidden shadow-lg border-4 border-transparent group-hover:border-white transition-all duration-300 transform group-hover:scale-105">
                         <img 
-                            src={person.image} 
-                            alt={person.name} 
+                            src={person.photo} 
+                            alt={person.username} 
                             className="w-full h-full object-cover"
                         />
                     </div>
                     <h3 className="mt-4 text-md font-medium text-gray-900 group-hover:text-slate-800 transition-colors">
-                        {person.name}
+                        {person.username}
                     </h3>
                 </div>
             ))}
