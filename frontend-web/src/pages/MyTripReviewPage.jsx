3f0a14f3d4fbd5ef4a40a9d82108fcd481948668
DimarsMars <dimasrangga620@gmail.com>
2025-12-24 16:00:50 +0800

    update gitignore

--- frontend-web/src/pages/MyTripReviewPage.jsx
+++ frontend-web/src/pages/MyTripReviewPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaChevronLeft, FaStar, FaBookmark } from "react-icons/fa";
 
