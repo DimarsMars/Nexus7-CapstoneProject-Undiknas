5e67fce2c9d78381139a97110c808ab3b0e1c27a
Arfiannn <arfianpndg@gmail.com>
2025-12-27 02:30:16 +0800

    fix(models): remove primary key constraint from FollowerID and FollowingID in Follow struct

--- journeys-backend/models/follow.go
+++ journeys-backend/models/follow.go
@@ -4,7 +4,7 @@ import "time"
 
 type Follow struct {
 	ID          uint `gorm:"primaryKey;column:id" json:"follow_id"`
-	FollowerID  uint `gorm:"primaryKey;column:follower_id" json:"follower_id"`
-	FollowingID uint `gorm:"primaryKey;column:following_id" json:"following_id"`
+	FollowerID  uint `gorm:"column:follower_id" json:"follower_id"`
+	FollowingID uint `gorm:"column:following_id" json:"following_id"`
 	CreatedAt   time.Time
 }
