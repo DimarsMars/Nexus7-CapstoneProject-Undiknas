6116749e5e1a976e912404ede4b1ce065bcfff18
DimarsMars <dimasrangga620@gmail.com>
2025-12-26 23:01:08 +0800

    update hilangkan runtrip

--- frontend-web/src/components/Navbar.jsx
+++ frontend-web/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
                             <Link smooth to="/homepage#" className={getLinkClass('/homepage', '')}>Home</Link>
                             <Link smooth to="/explore" className={getLinkClass('/explore')}>Explore</Link>
                             <Link smooth to="/maps" className={getLinkClass('/maps')}>Maps</Link>
-                            <Link smooth to="/runtrip" className={getLinkClass('/runtrip')}>Run Trip</Link>
+                            {/* <Link smooth to="/runtrip" className={getLinkClass('/runtrip')}>Run Trip</Link> */}
                             <Link smooth to="/history" className={getLinkClass('/history')}>History</Link>
                             <Link smooth to="/myprofile" className={getLinkClass('/myprofile')}>Profile</Link>
                         </>
