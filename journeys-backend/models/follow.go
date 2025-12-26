68a540d238e7c76025cc0a96fc98acfc5667c5ef
Arfiannn <arfianpndg@gmail.com>
2025-12-27 00:09:39 +0800

    fix(models): add missing ID field to Follow struct

--- journeys-backend/models/follow.go
+++ journeys-backend/models/follow.go
@@ -3,6 +3,7 @@ package models
 import "time"
 
 type Follow struct {
+	ID          uint `gorm:"primaryKey;column:id" json:"follow_id"`
 	FollowerID  uint `gorm:"primaryKey;column:follower_id" json:"follower_id"`
 	FollowingID uint `gorm:"primaryKey;column:following_id" json:"following_id"`
 	CreatedAt   time.Time
