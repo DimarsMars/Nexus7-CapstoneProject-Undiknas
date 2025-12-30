becb8cfb447377259b68d14a29947368005057d3
Arfiannn <arfianpndg@gmail.com>
2025-12-30 15:09:43 +0800

    feat(review): refactor CreatePlaceReview to use helper functions for type conversion and enhance image handling

--- journeys-backend/models/placereview.go
+++ journeys-backend/models/placereview.go
@@ -3,11 +3,17 @@ package models
 import "time"
 
 type PlaceReview struct {
-	ReviewID  uint      `gorm:"primaryKey;column:review_id" json:"review_id"`
-	UserID    uint      `gorm:"column:user_id" json:"user_id"`
-	RouteID   uint      `gorm:"column:route_id" json:"route_id"`
-	Rating    int       `gorm:"column:rating" json:"rating"`
-	Comment   string    `gorm:"column:comment" json:"comment"`
-	Image     []byte    `gorm:"column:image" json:"image"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
+	ReviewID  uint               `gorm:"primaryKey;column:review_id" json:"review_id"`
+	UserID    uint               `gorm:"column:user_id" json:"user_id"`
+	RouteID   uint               `gorm:"column:route_id" json:"route_id"`
+	Rating    int                `gorm:"column:rating" json:"rating"`
+	Comment   string             `gorm:"column:comment" json:"comment"`
+	CreatedAt time.Time          `gorm:"column:created_at" json:"created_at"`
+	Image     []PlaceReviewImage `gorm:"foreignKey:ReviewID;references:ReviewID" json:"image"`
+}
+
+type PlaceReviewImage struct {
+	ImageID  uint   `gorm:"primaryKey;column:image_id" json:"image_id"`
+	ReviewID uint   `gorm:"column:review_id" json:"review_id"`
+	Image    []byte `gorm:"column:image" json:"image"`
 }
